d568d69628e3b043ad26a61ddb294cb051b33231
devongrove <devon.grove@hotmail.com>
2014-11-04 20:54:32 -0800

    First commit
    So I can more easily see this repo appear on my account page

--- KEYBOARD-PROGRAM.rkt
+++ KEYBOARD-PROGRAM.rkt
@@ -2,8 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname KEYBOARD-PROGRAM) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")))))
 ; Electronic Keyboard Program
-;   By Thomas Franklin
-
+;   By Thomas Franklin, Nish Dara, Blaine Weeks, Devon Grove
 
 ; Required Packages
 (require rsound)
